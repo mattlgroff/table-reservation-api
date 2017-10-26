a47426daa0cfe7775315b92a92b713f4ff8c79e2
Matt Groff <mattlgroff@gmail.com>
2017-10-25 20:09:29 -0700

    Testing seperation of files

--- app.js
+++ app.js
@@ -1,59 +1,26 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const path = require("path");
-
 const amountOfTables = 5;
-const app = express();
-const port = process.env.PORT || 8080;
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 let tables = [];
 let waitlist = [];
 
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
+module.exports = {
+  newReservation: (req) => {
+    let newReservation = req.body;
 
-app.get("/tables", function(req, res) {
-  res.sendFile(path.join(__dirname, "tables.html"));
-});
+    if (tables.length < amountOfTables) {
+      tables.push(newReservation);
+      console.log("Added to tables: ");
+      
+    }
+    else {
+      waitlist.push(newReservation);
+      console.log("Added to waitlist: ");
+    }
 
-app.get("/reserve", function(req, res) {
-  res.sendFile(path.join(__dirname, "reserve.html"));
-});
-
-app.get("/api/waitlist", function(req, res) {
-  res.json(waitlist);
-});
-
-app.get("/api/tables", function(req, res) {
-  res.json(tables);
-});
-
-app.post("/api/clear", (req, res) => {
-  tables = waitlist;
-  waitlist = [];
-});
-
-app.post("/api/tables", function(req, res) {
-
-  let newReservation = req.body;
-
-  if (tables.length < amountOfTables) {
-    tables.push(newReservation);
-    console.log("Added to tables: ");
-    
+    console.log(newReservation);
+  },
+  clear: () => {
+    tables = waitlist;
+    waitlist = [];
   }
-  else {
-    waitlist.push(newReservation);
-    console.log("Added to waitlist: ");
-  }
-
-  console.log(newReservation);
-});
-
-app.listen(port, function() {
-  console.log("App listening on PORT " + port);
-});
+}
